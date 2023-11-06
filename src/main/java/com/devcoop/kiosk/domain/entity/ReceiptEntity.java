c6419fce63487d6942a7cffb24d4a2eeec2797b8
hyeonjoonpark <pjjoon1379@gmail.com>
2023-11-07 01:58:11 +0900

    FEAT: ReceiptEntity

---
+++ src/main/java/com/devcoop/kiosk/domain/entity/ReceiptEntity.java
@@ -0,0 +1,34 @@
+package com.devcoop.kiosk.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity(name = "receipt")
+@Table(name = "receipt")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReceiptEntity {
+    @Id
+    @Column(name = "number")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int number;
+    @Column(name = "dcm_sale_amt")
+    private int dcmSaleAmt;
+    @Column(name = "item_id")
+    private int itemId;
+    @Column(name = "sale_yn")
+    private Character saleYn;
+    @Column(name = "bill_num")
+    private int billNum;
+    @Column(name = "item_name")
+    private String itemName;
+    @Column(name = "sale_qty")
+    private Short saleQty;
+    @Column(name = "date")
+    private Date date;
+}
