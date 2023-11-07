e5ea594d13301b2323ecbcfc25ef433e656979c8
hyeonjoonpark <pjjoon1379@gmail.com>
2023-11-08 08:23:13 +0900

    FEAT: 구매하는 재고 수량 감소

--- src/main/java/com/devcoop/kiosk/domain/presentation/ItemSelectController.java
+++ src/main/java/com/devcoop/kiosk/domain/presentation/ItemSelectController.java
@@ -1,13 +1,12 @@
 package com.devcoop.kiosk.domain.presentation;
 
+import com.devcoop.kiosk.domain.entity.InventoryEntity;
 import com.devcoop.kiosk.domain.entity.ItemEntity;
 import com.devcoop.kiosk.domain.presentation.dto.ItemResponseDto;
+import com.devcoop.kiosk.domain.repository.InventoryRepository;
 import com.devcoop.kiosk.domain.repository.ItemRepository;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,24 +16,48 @@ import java.util.List;
 public class ItemSelectController {
 
     private final ItemRepository itemRepository;
+    private final InventoryRepository inventoryRepository;
 
-    public ItemSelectController(ItemRepository itemRepository) {
+    public ItemSelectController(ItemRepository itemRepository, InventoryRepository inventoryRepository) {
         this.itemRepository = itemRepository;
+        this.inventoryRepository = inventoryRepository;
     }
 
-    @GetMapping("/itemSelect")
-    public ResponseEntity<List<ItemResponseDto>> getItemsByBarcodes(@RequestParam List<String> barcodes) {
+    @PutMapping("/itemSelect")
+    public ResponseEntity<List<ItemResponseDto>> updateItemsByBarcodes(@RequestBody BarcodeRequest barcodeRequest) {
         List<ItemResponseDto> itemResponseDtos = new ArrayList<>();
 
-        for (String barcode : barcodes) {
-            ItemEntity item = itemRepository.findByBarcode(barcode);
-
-            if (item != null) {
-                ItemResponseDto itemResponse = new ItemResponseDto(item.getItemName(), item.getItemPrice());
-                itemResponseDtos.add(itemResponse);
+        try {
+            for (String barcode : barcodeRequest.getBarcodes()) {
+                ItemEntity item = itemRepository.findByBarcode(barcode);
+                if (item != null) {
+                    InventoryEntity inventory = inventoryRepository.findByItemId(item.getItemId());
+                    if (inventory != null) {
+                        int newQuantity = inventory.getQuantity() - 1;
+                        inventory.setQuantity(newQuantity);
+                        inventoryRepository.save(inventory);
+                    }
+                    ItemResponseDto itemResponse = new ItemResponseDto(item.getItemName(), item.getItemPrice());
+                    itemResponseDtos.add(itemResponse);
+                }
             }
-        }
 
-        return ResponseEntity.ok(itemResponseDtos);
+            return ResponseEntity.ok(itemResponseDtos);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+}
+
+class BarcodeRequest {
+    private List<String> barcodes;
+
+    public List<String> getBarcodes() {
+        return barcodes;
+    }
+
+    public void setBarcodes(List<String> barcodes) {
+        this.barcodes = barcodes;
     }
 }
