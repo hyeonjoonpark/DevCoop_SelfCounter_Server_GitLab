63ea8c27572ac82574bf279346524566130ff729
hyeonjoonpark <pjjoon1379@gmail.com>
2023-11-07 11:15:48 +0900

    FEAT: 바코드번호로 사용자 조회 API

---
+++ src/main/java/com/devcoop/kiosk/domain/presentation/dto/UserResponseDto.java
@@ -0,0 +1,14 @@
+package com.devcoop.kiosk.domain.presentation.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponseDto {
+    private int studentNumber;
+    private String studentName;
+    private int point;
+}
--- src/main/java/com/devcoop/kiosk/domain/repository/UserRepository.java
+++ src/main/java/com/devcoop/kiosk/domain/repository/UserRepository.java
@@ -2,7 +2,10 @@ package com.devcoop.kiosk.domain.repository;
 
 import com.devcoop.kiosk.domain.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
 
+    UserEntity findByCodeNumber(String codeNumber);
 }
