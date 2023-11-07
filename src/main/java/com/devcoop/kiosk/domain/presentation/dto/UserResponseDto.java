a0e0cbec5f4ba24f18dc9331007f7da9977e1181
hyeonjoonpark <pjjoon1379@gmail.com>
2023-11-07 22:40:10 +0900

    DELETE: UserResponseDto에서 studentNumber 삭제

--- src/main/java/com/devcoop/kiosk/domain/presentation/dto/UserResponseDto.java
+++ src/main/java/com/devcoop/kiosk/domain/presentation/dto/UserResponseDto.java
@@ -8,7 +8,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserResponseDto {
-    private int studentNumber;
     private String studentName;
     private int point;
 }
