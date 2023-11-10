db126c94a48ac3af81692dfdc69bb49fc38e161f
hyeonjoonpark <pjjoon1379@gmail.com>
2023-11-10 11:48:49 +0900

    FEAT: Kiosk Auth

--- src/main/java/com/devcoop/kiosk/domain/provider/TokenProvider.java
+++ src/main/java/com/devcoop/kiosk/domain/provider/TokenProvider.java
@@ -1,36 +1,54 @@
 package com.devcoop.kiosk.domain.provider;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.Base64;
 import java.util.Date;
 
-@Service
+@Getter
+@Component
+@RequiredArgsConstructor
 public class TokenProvider {
-    private static final String SECURITY_KEY = "jwtsecretkey!@";
+    private final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 120;
+    private final long REFRESH_TOKEN_EXPIRE_TIME = ACCESS_TOKEN_EXPIRE_TIME * 12 * 7;
 
-    // JWT 생성하는 메서드
-    public String createJwt(String userEmail) {
-        Date exprTime = Date.from(Instant.now().plus(10, ChronoUnit.MINUTES));
-        return Jwts.builder() // builder를 이용해서 생성
-                .signWith(SignatureAlgorithm.HS512, SECURITY_KEY) // 암호화 알고리즘, 시크릿 키
-                .setSubject(userEmail) // JWT의 제목
-                .setIssuedAt(new Date()) // 생성날짜
-                .setExpiration(exprTime) // 만료날짜
-                .compact(); // 생성!
+    @AllArgsConstructor
+    private enum TokenType {
+        ACCESS_TOKEN("accessToken");
+        String value;
     }
 
-    // JWT 검증 메서드
-    public String validateJwt(String token) {
-        Claims claims = Jwts.parser()
-                .setSigningKey(SECURITY_KEY) // token을 SECURITY_KEY를 이용해 파싱
-                .parseClaimsJws(token)
-                .getBody();
+    @AllArgsConstructor
+    private enum TokenClaimName {
+        USER_ID("id"),
+        TOKEN_TYPE("tokenType");
+        String value;
+    }
 
-        return claims.getSubject(); // token을 파싱해서 토큰 생성할 때 넣은 userEmail을 가져올 수 있다
+    private Key getSignInKey(String secretKey) {
+        return Keys.secretKeyFor(SignatureAlgorithm.HS512);
+    }
+
+    public String createJwt(String codeNumber) {
+        String secretKey = "KiOSkSEcrET";
+        return Jwts.builder()
+                .signWith(getSignInKey(secretKey))
+                .setSubject(codeNumber)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(this.ACCESS_TOKEN_EXPIRE_TIME))
+                .compact();
     }
 }
--- src/main/java/com/devcoop/kiosk/domain/service/AuthService.java
+++ src/main/java/com/devcoop/kiosk/domain/service/AuthService.java
@@ -8,31 +8,42 @@ import com.devcoop.kiosk.domain.provider.TokenProvider;
 import com.devcoop.kiosk.domain.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.Base64;
 
 @Service
 public class AuthService {
+    private final UserRepository userRepository;
+    private final TokenProvider tokenProvider;
+
     @Autowired
-    UserRepository userRepository;
-    @Autowired
-    TokenProvider tokenProvider;
+    public AuthService(UserRepository userRepository, TokenProvider tokenProvider) {
+        this.userRepository = userRepository;
+        this.tokenProvider = tokenProvider;
+    }
 
     public ResponseDto<LoginResponseDto> login(LoginRequestDto dto) {
         String codeNumber = dto.getCodeNumber();
         String pin = dto.getPin();
 
         try {
-            boolean existed =  userRepository.existsByCodeNumberAndPin(codeNumber, pin);
-            if(!existed) {
+            boolean exists = userRepository.existsByCodeNumberAndPin(codeNumber, pin);
+            System.out.println(exists);
+            if (!exists) {
                 return ResponseDto.setFailed("Login Info is Wrong");
             }
         } catch (Exception e) {
+            System.out.println(e);
             return ResponseDto.setFailed("Database Error");
         }
 
-        UserEntity userEntity = null;
+        UserEntity userEntity;
+
         try {
-            // 값이 존재하면
-            userEntity = userRepository.findById(Integer.valueOf(codeNumber)).get(); // 사용자 바코드를 가져옴
+            userEntity = userRepository.findByCodeNumber(codeNumber);
+            System.out.println(userEntity);
+            if (userEntity == null) {
+                return ResponseDto.setFailed("User not found");
+            }
         } catch(Exception e) {
             return ResponseDto.setFailed("Database Error");
         }
@@ -40,9 +51,20 @@ public class AuthService {
         userEntity.setCodeNumber("");
 
         String token = tokenProvider.createJwt(codeNumber);
-        int exprTime = 600000; // 10분
+        System.out.println(token);
+        int exprTime = 600000; // 10 minutes
 
         LoginResponseDto loginResponseDto = new LoginResponseDto(token, exprTime, userEntity);
+        System.out.println(loginResponseDto);
         return ResponseDto.setSuccess("Login Success", loginResponseDto);
+    }
+
+    public String encodeBase64(String input) {
+        return Base64.getEncoder().encodeToString(input.getBytes());
+    }
+
+    public String decodeBase64(String encodedInput) {
+        byte[] decodedBytes = Base64.getDecoder().decode(encodedInput);
+        return new String(decodedBytes);
     }
 }
