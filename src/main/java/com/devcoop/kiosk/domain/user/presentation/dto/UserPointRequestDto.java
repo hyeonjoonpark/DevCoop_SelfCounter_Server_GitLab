9809b09003d5e5435970bc04d273e155d5115ddc
hyeonjoonpark <pjjoon1379@gmail.com>
2024-03-12 02:23:22 +0900

    FIX: Payments API

--- src/main/java/com/devcoop/kiosk/domain/user/presentation/dto/UserPointRequestDto.java
+++ src/main/java/com/devcoop/kiosk/domain/user/presentation/dto/UserPointRequestDto.java
@@ -2,9 +2,10 @@ package com.devcoop.kiosk.domain.user.presentation.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@AllArgsConstructor
+@NoArgsConstructor
 public class UserPointRequestDto {
     private String codeNumber;
     private int totalPrice;
