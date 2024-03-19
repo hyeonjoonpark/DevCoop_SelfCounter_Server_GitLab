de43ec7aa58ce3355bac90538880422f0274db10
hyeonjoonpark <pjjoon1379@gmail.com>
2024-03-20 02:22:39 +0900

    REFACTOR: payments dto record

--- src/main/java/com/devcoop/kiosk/domain/item/presentation/SelectItemController.java
+++ src/main/java/com/devcoop/kiosk/domain/item/presentation/SelectItemController.java
@@ -1,6 +1,6 @@
 package com.devcoop.kiosk.domain.item.presentation;
 
-import com.devcoop.kiosk.domain.item.presentation.dto.ItemResponseDto;
+import com.devcoop.kiosk.domain.item.presentation.dto.ItemResponse;
 import com.devcoop.kiosk.domain.item.service.ItemSelectService;
 import com.devcoop.kiosk.global.exception.GlobalException;
 import lombok.RequiredArgsConstructor;
@@ -16,9 +16,9 @@ import java.util.List;
 public class SelectItemController {
   private final ItemSelectService itemSelectService;
   @GetMapping("/itemSelect")
-  public List<ItemResponseDto> getItemByBarcode(@RequestParam List<String> barcodes) throws GlobalException {
+  public List<ItemResponse> getItemByBarcode(@RequestParam List<String> barcodes) throws GlobalException {
     log.info("barcodes = {}", barcodes.toString());
-    List<ItemResponseDto> itemResponseDtos = itemSelectService.get(barcodes);
-    return itemResponseDtos;
+    List<ItemResponse> itemResponses = itemSelectService.get(barcodes);
+    return itemResponses;
   }
 }
