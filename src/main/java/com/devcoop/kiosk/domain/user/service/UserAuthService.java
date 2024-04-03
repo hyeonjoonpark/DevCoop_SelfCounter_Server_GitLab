3dc6da7b648e0d293fc06c5e7f4079706e2f1cdc
hyunjoon park <105212959+hyeonjoonpark@users.noreply.github.com>
2024-04-03 12:22:59 +0900

    HOTFIX: 핀번호 에러 해결

--- src/main/java/com/devcoop/kiosk/domain/user/service/UserAuthService.java
+++ src/main/java/com/devcoop/kiosk/domain/user/service/UserAuthService.java
@@ -38,7 +38,6 @@ public class UserAuthService {
       throw new GlobalException(ErrorCode.USER_NOT_FOUND);
     }
 
-    // 패스워드 비교해서 에러 핸들링 해주는 로직만 추가하면 에러가 발생함
     if(!bCryptPasswordEncoder.matches(pin, user.getPin())) {
       throw new RuntimeException("핀 번호가 옳지 않습니다");
     }
