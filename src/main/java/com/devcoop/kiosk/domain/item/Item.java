8049230b2689daa810124308421334131b950733
hyunjoon park <105212959+hyeonjoonpark@users.noreply.github.com>
2024-04-09 09:16:38 +0900

    ROLLBACK

--- src/main/java/com/devcoop/kiosk/domain/item/Item.java
+++ src/main/java/com/devcoop/kiosk/domain/item/Item.java
@@ -1,45 +1,29 @@
 package com.devcoop.kiosk.domain.item;
 
-import com.devcoop.kiosk.domain.receipt.KioskReceipt;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.util.List;
-
 @Entity
-@Getter
-@ToString
+@Getter @ToString
 @Table
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Item {
-  @Id
-  @Column(name = "item_id")
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Integer itemId;
-  @Column(name = "barcode")
-  private String barcode;
-  @Column(name = "item_name")
-  private String itemName;
-  @Column(name = "item_price")
-  private int itemPrice;
+    @Id
+    @Column(name = "item_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer itemId;
+    @Column(name = "barcode")
+    private String barcode;
+    @Column(name = "item_name")
+    private String itemName;
+    @Column(name = "item_price")
+    private int itemPrice;
 
-  @OneToMany(
-    mappedBy = "item",
-    cascade = CascadeType.ALL,
-    orphanRemoval = true
-  )
-  private List<KioskReceipt> receipts;
-
-  public void addReceipt(KioskReceipt receipt) {
-    receipt.setItem(this);
-    receipts.add(receipt);
-  }
-
-  @Builder
-  public Item(String barcode, String itemName, int itemPrice, List<KioskReceipt> receipts) {
-    this.barcode = barcode;
-    this.itemName = itemName;
-    this.itemPrice = itemPrice;
-    this.receipts = receipts;
-  }
+    @Builder
+    public Item(int itemId, String barcode, String itemName, int itemPrice) {
+        this.itemId = itemId;
+        this.barcode = barcode;
+        this.itemName = itemName;
+        this.itemPrice = itemPrice;
+    }
 }
--- src/main/java/com/devcoop/kiosk/domain/receipt/KioskReceipt.java
+++ src/main/java/com/devcoop/kiosk/domain/receipt/KioskReceipt.java
@@ -1,6 +1,5 @@
 package com.devcoop.kiosk.domain.receipt;
 
-import com.devcoop.kiosk.domain.item.Item;
 import com.devcoop.kiosk.domain.receipt.types.ReceiptType;
 import jakarta.persistence.*;
 import lombok.*;
@@ -9,44 +8,38 @@ import java.time.LocalDate;
 
 @Entity
 @Table(name = "kiosk_receipts")
-@Getter
-@ToString
+@Getter @ToString
 @NoArgsConstructor
 public class KioskReceipt {
-  @Id
-  @Column(name = "number")
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private int number;
-  @Column(name = "dcm_sale_amt") // 팔린 금액
-  private int dcmSaleAmt;
+    @Id
+    @Column(name = "number")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int number;
+    @Column(name = "dcm_sale_amt") // 팔린 금액
+    private int dcmSaleAmt;
+    @Column(name = "item_id") // 아이템 Id
+    private String itemId;
+    @Column(name = "sale_yn") // 팔렸으면 Y 안 팔리면 N
+    @Enumerated(EnumType.STRING)
+    private ReceiptType saleYn;
+    @Column(name = "user_id") // 영수증 번호
+    private String userId;
+    @Column(name = "item_name") // 아이템 이름
+    private String itemName;
+    @Column(name = "sale_qty") // 팔린 양
+    private Short saleQty;
+    @Column(name = "date") // 팔린 날짜
+    private LocalDate date;
 
-  @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "item_id")
-  private Item item;
-  public void setItem(Item item) {
-    this.item = item;
-  }
-
-  @Column(name = "sale_yn") // 팔렸으면 Y 안 팔리면 N
-  @Enumerated(EnumType.STRING)
-  private ReceiptType saleYn;
-  @Column(name = "user_id") // 영수증 번호
-  private String userId;
-  @Column(name = "item_name") // 아이템 이름
-  private String itemName;
-  @Column(name = "sale_qty") // 팔린 양
-  private Short saleQty;
-  @Column(name = "date") // 팔린 날짜
-  private LocalDate date;
-
-  @Builder
-  public KioskReceipt(int dcmSaleAmt, Item item, ReceiptType saleYn, String userId, String itemName, Short saleQty, LocalDate date) {
-    this.dcmSaleAmt = dcmSaleAmt;
-    this.item = item;
-    this.saleYn = saleYn;
-    this.userId = userId;
-    this.itemName = itemName;
-    this.saleQty = saleQty;
-    this.date = date;
-  }
+    @Builder
+    public KioskReceipt(int number, int dcmSaleAmt, String itemId, ReceiptType saleYn, String userId, String itemName, Short saleQty, LocalDate date) {
+        this.number = number;
+        this.dcmSaleAmt = dcmSaleAmt;
+        this.itemId = itemId;
+        this.saleYn = saleYn;
+        this.userId = userId;
+        this.itemName = itemName;
+        this.saleQty = saleQty;
+        this.date = date;
+    }
 }
