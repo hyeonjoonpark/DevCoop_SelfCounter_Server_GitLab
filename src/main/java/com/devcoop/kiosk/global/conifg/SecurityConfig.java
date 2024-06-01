43aafb5521f8cf5c245af849dcf0423107bcb3c5
hyeonjoonpark <pjjoon1379@gmail.com>
2024-06-02 02:37:45 +0900

    ADD: security config에 행사상품 API 추가

--- src/main/java/com/devcoop/kiosk/global/conifg/SecurityConfig.java
+++ src/main/java/com/devcoop/kiosk/global/conifg/SecurityConfig.java
@@ -48,16 +48,17 @@ public class SecurityConfig {
       .httpBasic(AbstractHttpConfigurer::disable)
       .authorizeHttpRequests(
         request -> request
-          .requestMatchers(HttpMethod.POST, "/kiosk/auth/signIn").permitAll()
-          .requestMatchers(HttpMethod.POST, "/kiosk/save/receipt").permitAll()
-          .requestMatchers(HttpMethod.GET, "/kiosk/item/ai/suggest").permitAll()
-          .requestMatchers(HttpMethod.GET, "/kiosk/item/top/list").permitAll()
-          .requestMatchers(HttpMethod.PUT, "/kiosk/auth/pwChange").permitAll()
-          .requestMatchers(HttpMethod.POST, "/kiosk/save/paylog").authenticated()
-          .requestMatchers(HttpMethod.POST, "/kiosk/save/receipt").authenticated()
-          .requestMatchers(HttpMethod.POST, "/kiosk/executePayments").authenticated()
-          .requestMatchers(HttpMethod.GET, "/itemSelect").authenticated()
-          .requestMatchers(HttpMethod.PUT, "/kiosk/pay").authenticated()
+          .requestMatchers(HttpMethod.POST, "/kiosk/auth/signIn").permitAll() // 로그인 API
+          .requestMatchers(HttpMethod.POST, "/kiosk/save/receipt").permitAll() // 영수증 저장 API
+          .requestMatchers(HttpMethod.GET, "/kiosk/item/ai/suggest").permitAll() // 상품추천 API
+          .requestMatchers(HttpMethod.GET, "/kiosk/event-item/get-item").permitAll() // 행사상품 조회 API
+          .requestMatchers(HttpMethod.GET, "/kiosk/item/top/list").permitAll() // 인기상품 조회 API
+          .requestMatchers(HttpMethod.PUT, "/kiosk/auth/pwChange").permitAll() // 비밀번호 변경 API
+          .requestMatchers(HttpMethod.POST, "/kiosk/save/paylog").authenticated() // 결제정보 저장 API
+          .requestMatchers(HttpMethod.POST, "/kiosk/save/receipt").authenticated() // 영수증 저장 API
+          .requestMatchers(HttpMethod.POST, "/kiosk/executePayments").authenticated() // 결제, 결제정보 저장, 영수증 저장 모두 하나의 트랜잭션 안에서 실행되는 API
+          .requestMatchers(HttpMethod.GET, "/itemSelect").authenticated() // 상품정보 불러오기 API
+          .requestMatchers(HttpMethod.PUT, "/kiosk/pay").authenticated() // 결제 API
           .anyRequest().permitAll()
       )
       .sessionManagement(
