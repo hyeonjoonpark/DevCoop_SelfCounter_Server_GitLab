647cecc5996414c49d6af833f15a04a2880e5e62
hyeonjoonpark <pjjoon1379@gmail.com>
2024-06-21 09:39:41 +0900

    FIX: Entity

--- src/main/java/com/devcoop/kiosk/domain/user/User.java
+++ src/main/java/com/devcoop/kiosk/domain/user/User.java
@@ -2,57 +2,62 @@ package com.devcoop.kiosk.domain.user;
 
 import com.devcoop.kiosk.domain.user.types.Role;
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
 
 @Entity
 @Table(name = "users")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
-  @Id
-  private int studentNumber;
-  private String codeNumber;
-  private String pin;
-  private String studentName;
-  private String email;
-  private String password;
-  private int point;
-  private Short isAdmin;
-  private Short isCoop;
-  private String type;
-  private String pointStatus;
-  private String refToken;
+    @Id
+    private int studentNumber;
+    private String codeNumber;
+    private String pin;
+    private String studentName;
+    private String email;
+    private String password;
+    @ColumnDefault("0")
+    private int point;
+    private Short isAdmin;
+    private Short isCoop;
+    private String type;
+    private String pointStatus;
+    private String refToken;
 
-  @Enumerated(EnumType.STRING)
-  @Column(nullable = false)
-  private Role role = Role.ROLE_USER_NOT_COOP; // default 값은 ROLE_USER
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role = Role.ROLE_USER_NOT_COOP; // default 값은 ROLE_USER_NOT_COOP
 
-  @Builder
-  public User(
-    int studentNumber, String codeNumber, String pin, String studentName,
-    String email, String password, int point, Short isAdmin,
-    Short isCoop, String type, String pointStatus, String refToken, Role role
-  ) {
-    this.studentNumber = studentNumber;
-    this.codeNumber = codeNumber;
-    this.pin = pin;
-    this.studentName = studentName;
-    this.email = email;
-    this.password = password;
-    this.point = point;
-    this.isAdmin = isAdmin;
-    this.isCoop = isCoop;
-    this.type = type;
-    this.pointStatus = pointStatus;
-    this.refToken = refToken;
-    this.role = role;
-  }
+    @Builder
+    public User(
+            int studentNumber, String codeNumber, String pin, String studentName,
+            String email, String password, int point, Short isAdmin,
+            Short isCoop, String type, String pointStatus, String refToken, Role role
+    ) {
+        this.studentNumber = studentNumber;
+        this.codeNumber = codeNumber;
+        this.pin = pin;
+        this.studentName = studentName;
+        this.email = email;
+        this.password = password;
+        this.point = point;
+        this.isAdmin = isAdmin;
+        this.isCoop = isCoop;
+        this.type = type;
+        this.pointStatus = pointStatus;
+        this.refToken = refToken;
+        this.role = role;
+    }
 
-  public void setPoint(int point) {
-    this.point = point;
-  }
+    public void setPoint(int point) {
+        this.point = point;
+    }
 
-  public void update(String pin) {
-    this.pin = pin;
-  }
+    public void update(String pin) {
+        this.pin = pin;
+    }
 }
