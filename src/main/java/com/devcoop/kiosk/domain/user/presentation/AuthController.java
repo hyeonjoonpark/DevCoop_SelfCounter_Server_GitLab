f41e1f91664dd24a9c8a67b36a62ea2aa23899d0
hyeonjoonpark <pjjoon1379@gmail.com>
2024-07-09 17:27:34 +0900

    FEAT: reset pin

--- src/main/java/com/devcoop/kiosk/domain/user/presentation/AuthController.java
+++ src/main/java/com/devcoop/kiosk/domain/user/presentation/AuthController.java
@@ -8,25 +8,34 @@ import com.devcoop.kiosk.global.exception.GlobalException;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
-@Tag(name= "SignIn", description = "SignIn API")
+@Tag(name = "SignIn", description = "SignIn API")
 public class AuthController {
-  private final UserAuthService userAuthService;
+    private final UserAuthService userAuthService;
 
-  @PostMapping("/signIn")
-  @Operation(summary = "login", description = "로그인")
-  public LoginResponse login(@RequestBody LoginRequest loginRequest) throws GlobalException {
-    LoginResponse result = userAuthService.login(loginRequest);
-    return result;
-  }
+    @PostMapping("/signIn")
+    @Operation(summary = "login", description = "로그인")
+    public LoginResponse login(@RequestBody LoginRequest loginRequest) throws GlobalException {
+        LoginResponse result = userAuthService.login(loginRequest);
+        return result;
+    }
 
-  @PutMapping("/pwChange")
-  @Operation(summary = "update password", description = "비밀번호 변경")
-  public void changePassword(@RequestBody PinChangeRequest pinChangeRequest) throws GlobalException {
-    userAuthService.changePassword(pinChangeRequest);
-  }
+    @PutMapping("/pwChange")
+    @Operation(summary = "update password", description = "비밀번호 변경")
+    public void changePassword(@RequestBody PinChangeRequest pinChangeRequest) throws GlobalException {
+        userAuthService.changePassword(pinChangeRequest);
+    }
+
+    @PutMapping("/pw-reset")
+    @Operation(summary = "reset password", description = "비밀번호 초기화")
+    public ResponseEntity<?> resetPassword(@RequestBody String codeNumber) throws GlobalException {
+        userAuthService.resetPassword(codeNumber);
+        return ResponseEntity.status(HttpStatus.OK).body("성공적으로 핀번호를 초기화하였습니다.");
+    }
 }
--- src/main/java/com/devcoop/kiosk/domain/user/service/UserAuthService.java
+++ src/main/java/com/devcoop/kiosk/domain/user/service/UserAuthService.java
@@ -4,10 +4,10 @@ import com.devcoop.kiosk.domain.user.User;
 import com.devcoop.kiosk.domain.user.presentation.dto.LoginRequest;
 import com.devcoop.kiosk.domain.user.presentation.dto.LoginResponse;
 import com.devcoop.kiosk.domain.user.presentation.dto.PinChangeRequest;
-import com.devcoop.kiosk.global.utils.security.JwtUtil;
 import com.devcoop.kiosk.domain.user.repository.UserRepository;
 import com.devcoop.kiosk.global.exception.GlobalException;
 import com.devcoop.kiosk.global.exception.enums.ErrorCode;
+import com.devcoop.kiosk.global.utils.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,71 +17,83 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 public class UserAuthService {
-  private final UserRepository userRepository;
-  private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final Long exprTime = 1000 * 60 * 60L;
+    @Value("${jwt.secret}")
+    private String secretKey;
 
-  @Value("${jwt.secret}")
-  private String secretKey;
+    @Transactional
+    public LoginResponse login(LoginRequest dto) throws GlobalException {
+        String codeNumber = dto.getCodeNumber();
+        String pin = dto.getPin();
 
-  private Long exprTime = 1000 * 60 * 60L;
+        User user = userRepository.findByCodeNumber(codeNumber);
 
-  @Transactional
-  public LoginResponse login(LoginRequest dto) throws GlobalException {
-    String codeNumber = dto.getCodeNumber();
-    String pin = dto.getPin();
+        if (user == null) {
+            throw new GlobalException(ErrorCode.USER_NOT_FOUND);
+        }
 
-    User user = userRepository.findByCodeNumber(codeNumber);
+        System.out.println("user = " + user.getPin());
 
-    if (user == null) {
-      throw new GlobalException(ErrorCode.USER_NOT_FOUND);
+        if (!bCryptPasswordEncoder.matches(pin, user.getPin())) {
+            throw new RuntimeException("핀 번호가 옳지 않습니다");
+        }
+
+        String token = JwtUtil.createJwt(codeNumber, secretKey, exprTime);
+
+        System.out.println("user.getStudentName = " + user.getStudentName());
+
+        LoginResponse loginResponse = LoginResponse.builder()
+                .token(token)
+                .studentNumber(user.getStudentNumber())
+                .codeNumber(user.getCodeNumber())
+                .studentName(user.getStudentName())
+                .point(user.getPoint())
+                .build();
+
+        return loginResponse;
+
     }
 
-    System.out.println("user = " + user.getPin());
+    @Transactional
+    public void changePassword(PinChangeRequest dto) throws GlobalException {
+        String codeNumber = dto.getCodeNumber();
+        String pin = dto.getPin();
+        String newPin = dto.getNewPin();
 
-    if(!bCryptPasswordEncoder.matches(pin, user.getPin())) {
-      throw new RuntimeException("핀 번호가 옳지 않습니다");
+        User user = userRepository.findByCodeNumber(codeNumber);
+        System.out.println("user = " + user);
+
+        if (user == null) {
+            throw new GlobalException(ErrorCode.USER_NOT_FOUND);
+        }
+
+        if (!bCryptPasswordEncoder.matches(pin, user.getPin())) {
+            throw new RuntimeException("현재핀 번호가 옳지 않습니다");
+        }
+
+        if (bCryptPasswordEncoder.matches(newPin, user.getPin())) {
+            throw new RuntimeException("현재 핀번호와 다른 핀번호를 입력해주세요");
+        }
+
+        String encodedPin = bCryptPasswordEncoder.encode(newPin);
+
+        user.update(encodedPin);
+        userRepository.save(user);
     }
 
-    String token = JwtUtil.createJwt(codeNumber, secretKey, exprTime);
+    @Transactional
+    public void resetPassword(String codeNumber) throws GlobalException {
+        String resetPassword = "1234";
 
-    System.out.println("user.getStudentName = " + user.getStudentName());
+        User user = userRepository.findByCodeNumber(codeNumber);
 
-    LoginResponse loginResponse = LoginResponse.builder()
-      .token(token)
-      .studentNumber(user.getStudentNumber())
-      .codeNumber(user.getCodeNumber())
-      .studentName(user.getStudentName())
-      .point(user.getPoint())
-      .build();
+        if (user == null) {
+            throw new GlobalException(ErrorCode.USER_NOT_FOUND);
+        }
 
-    return loginResponse;
-
-  }
-
-  @Transactional
-  public void changePassword(PinChangeRequest dto) throws GlobalException {
-    String codeNumber = dto.getCodeNumber();
-    String pin = dto.getPin();
-    String newPin = dto.getNewPin();
-
-    User user = userRepository.findByCodeNumber(codeNumber);
-    System.out.println("user = " + user);
-
-    if (user == null) {
-      throw new GlobalException(ErrorCode.USER_NOT_FOUND);
+        user.update(resetPassword);
+        userRepository.save(user);
     }
-
-    if(!bCryptPasswordEncoder.matches(pin, user.getPin())) {
-      throw new RuntimeException("현재핀 번호가 옳지 않습니다");
-    }
-
-    if(bCryptPasswordEncoder.matches(newPin, user.getPin())) {
-      throw new RuntimeException("현재 핀번호와 다른 핀번호를 입력해주세요");
-    }
-
-    String encodedPin = bCryptPasswordEncoder.encode(newPin);
-
-    user.update(encodedPin);
-    userRepository.save(user);
-  }
 }
